41836546cecde9fc63d666d24cfff5b7d6a9a74c
Jedrzej Rochala <rochala@ctemplar.com>
2020-06-09 16:59:33 +0200

    add data fetch from market

--- new_predict.py
+++ new_predict.py
@@ -7,39 +7,48 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from random import randrange
+import requests as req
+
+norm = {}
 
 
-# load training data
-df = pd.read_csv("btcdata.csv", header=0)
-df["timestamp"] = pd.to_datetime(df["timestamp"]).dt.strftime("%Y%m%d")
-df.head()
-df["ratio"] = df["close"] / df["volume"]
-norm = {
-    "close": [df["close"].min(), df["close"].max()],
-    "volume": [df["volume"].min(), df["volume"].max()],
-    "ratio": [df["ratio"].min(), df["ratio"].max()],
-}
-df["close"] = (df["close"] - df["close"].min()) / (
-    df["close"].max() - df["close"].min()
-)
-df["volume"] = (df["volume"] - df["volume"].min()) / (
-    df["volume"].max() - df["volume"].min()
-)
-df["ratio"] = (df["ratio"] - df["ratio"].min()) / (
-    df["ratio"].max() - df["ratio"].min()
-)
-df["price_diff"] = df["close"].diff()
-df["volume_diff"] = df["volume"].diff()
+def fetch_data(currency, startTime, endTime):
+    limit = min(1000, int((endTime - startTime) / 86400 + 1))
+    data = req.get(
+        "https://www.bitstamp.net/api/v2/ohlc/{0}/".format(currency),
+        params={
+            "start": startTime - 86400,
+            "end": endTime,
+            "step": 86400,
+            "limit": limit,
+        },
+    )
+    return data.json()
 
 
-df.set_index("timestamp", inplace=True)
-df.sort_values(["timestamp"], ascending=False)
-df.head()
-
-
-# data prep
-X = np.array(df[["close", "volume", "price_diff", "volume_diff", "ratio"]])[1:-1]
-y = np.array(df[["close", "volume"]])[2:]
+def parse_data(data):
+    global norm
+    df = pd.DataFrame.from_records(data["data"]["ohlc"])
+    df.set_index("timestamp", inplace=True)
+    df = df.apply(pd.to_numeric, errors="coerce")
+    df["ratio"] = df["close"] / df["volume"]
+    norm = {
+        "close": [df["close"].min(), df["close"].max()],
+        "volume": [df["volume"].min(), df["volume"].max()],
+        "ratio": [df["ratio"].min(), df["ratio"].max()],
+    }
+    df["close"] = (df["close"] - df["close"].min()) / (
+        df["close"].max() - df["close"].min()
+    )
+    df["volume"] = (df["volume"] - df["volume"].min()) / (
+        df["volume"].max() - df["volume"].min()
+    )
+    df["ratio"] = (df["ratio"] - df["ratio"].min()) / (
+        df["ratio"].max() - df["ratio"].min()
+    )
+    df["price_diff"] = df["close"].diff()
+    df["volume_diff"] = df["volume"].diff()
+    return df
 
 
 def denormalize(value):
@@ -135,38 +144,64 @@ def future_values(next_days, X_val, knn):
     return y_sim
 
 
-def simulate(iterations, neighbors, future_predictions):
+def simulate(iterations, neighbors, future_predictions, currency, start, end):
     # data split
+    data = fetch_data(currency, start, end)
+    df = parse_data(data)
+
+    X = np.array(df[["close", "volume", "price_diff", "volume_diff", "ratio"]])[1:]
+    y = np.array(df[["close", "volume"]])[1:]
+
     random = randrange(100000000)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.10, random_state=random
-    )
-
     knn = KNeighborsRegressor(neighbors)
-    knn.fit(X_train, y_train)
+    knn.fit(X, y)
 
-    y_ = knn.predict(X_test)
+    y_ = knn.predict(X[-int(1 - len(y) * 0.8) :])
 
-    mse = mean_squared_error(y_test, y_)
-    mae = mean_absolute_error(y_test, y_)
+    mse = mean_squared_error(y[-int(1 - len(y) * 0.8) :], y_)
+    mae = mean_absolute_error(y[-int(1 - len(y) * 0.8) :], y_)
 
     y_future = np.array(future_values(future_predictions, X[[-1]], knn))
 
-    plot(y_test, y_, y_future)
+    plot(
+        y[-int(1 - len(y) * 0.8) :],
+        y_,
+        y_future,
+        "Real prices 1 simulation without noise",
+    )
 
     y_mean = y_
-    y_test_mean = y_test
+    y_test_mean = y[-int(1 - len(y) * 0.8) :]
 
     print(
         f"Mean squared error: {mse}   Mean absolute error:{mae}   Random seed: {random}"
     )
+    print(
+        f"Mean Close: {np.mean(y_future, axis=(0))[0]}  Mean Volume: {np.mean(y_future, axis=(0))[1]}\n"
+        + f"Median Close: {np.median(y_future, axis=(0))[0]}  Median Volume: {np.mean(y_future, axis=(0))[1]}\n"
+        + f"Std Close: {np.std(y_future, axis=(0))[0]}  Std Volume: {np.mean(y_future, axis=(0))[1]}\n"
+    )
 
     mse = 0
     mae = 0
+    print("\n")
 
     for i in range(0, iterations - 1):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10)
+
+        # Reset
+
+        X_train = X[: int(len(X) * 0.8)]
+        X_test = X[: int(1 - len(X) * 0.8)]
+        y_train = y[: int(len(y) * 0.8)]
+        y_test = y[: int(1 - len(y) * 0.8)]
+
+        # Noise
+
+        X_train += X_train * np.random.normal(0, 0.02, size=(len(X_train), 5))
+        X_test += X_test * np.random.normal(0, 0.02, size=(len(X_test), 5))
+        y_train += y_train * np.random.normal(0, 0.02, size=(len(y_train), 2))
+        y_test += y_test * np.random.normal(0, 0.02, size=(len(y_test), 2))
 
         knn.fit(X_train, y_train)
         y_ = knn.predict(X_test)
@@ -185,81 +220,8 @@ def simulate(iterations, neighbors, future_predictions):
     print(
         f"Mean squared error: {mse/iterations}   Mean absolute error:{mae/iterations}"
     )
-    plot(y_mean, y_test_mean, y_future)
-    print(
-        f"Mean Close: {np.mean(y_future, axis=(0))[0]}  Mean Volume: {np.mean(y_future, axis=(0))[1]}\n"
-        + f"Median Close: {np.median(y_future, axis=(0))[0]}  Median Volume: {np.mean(y_future, axis=(0))[1]}\n"
-        + f"Std Close: {np.std(y_future, axis=(0))[0]}  Std Volume: {np.mean(y_future, axis=(0))[1]}\n"
-    )
-
-
-def simulate(iterations, neighbors, future_predictions):
-    # data split
-    random = randrange(100000000)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.20, random_state=random
-    )
-
-    knn = KNeighborsRegressor(neighbors)
-    knn.fit(X, y)
-
-    y_ = knn.predict(X[-len(y_test) :])
-
-    mse = mean_squared_error(y[-len(y_test) :], y_)
-    mae = mean_absolute_error(y[-len(y_test) :], y_)
-
-    y_future = np.array(future_values(future_predictions, X[[-1]], knn))
-
     plot(
-        y[-len(y_test) :],
-        y_,
-        y_future,
-        "Real prices 1 simulation without randomizing arrays",
-    )
-
-    y_mean = y_
-    y_test_mean = y[-len(y_test) :]
-
-    print(
-        f"Mean squared error: {mse}   Mean absolute error:{mae}   Random seed: {random}"
-    )
-    print(
-        f"Mean Close: {np.mean(y_future, axis=(0))[0]}  Mean Volume: {np.mean(y_future, axis=(0))[1]}\n"
-        + f"Median Close: {np.median(y_future, axis=(0))[0]}  Median Volume: {np.mean(y_future, axis=(0))[1]}\n"
-        + f"Std Close: {np.std(y_future, axis=(0))[0]}  Std Volume: {np.mean(y_future, axis=(0))[1]}\n"
-    )
-
-    mse = 0
-    mae = 0
-    print("\n")
-
-    for i in range(0, iterations - 1):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-
-        # knn.fit(X_train, y_train)
-        y_ = knn.predict(X_test)
-
-        mse += mean_squared_error(y_test, y_)
-        mae += mean_absolute_error(y_test, y_)
-
-        y_mean += y_
-        y_test_mean += y_test
-        y_future += np.array(future_values(future_predictions, X[[-1]], knn))
-
-    y_mean /= iterations
-    y_test_mean /= iterations
-    y_future /= iterations
-
-    print(
-        f"Mean squared error: {mse/iterations}   Mean absolute error:{mae/iterations}"
-    )
-
-    plot(
-        y_mean,
-        y_test_mean,
-        y_future,
-        "Mean prices from 100 simulations with randomizing arrays",
+        y_mean, y_test_mean, y_future, "Mean prices from 100 simulations with noise",
     )
     print(
         f"Mean Close: {np.mean(y_future, axis=(0))[0]}  Mean Volume: {np.mean(y_future, axis=(0))[1]}\n"
@@ -268,5 +230,6 @@ def simulate(iterations, neighbors, future_predictions):
     )
 
 
+# simulate(neighbors, iterations, days for prediction, currency <btcusd>, timestamp start, timestamp end)
 if __name__ == "__main__":
-    simulate(8, 100, 10)
+    simulate(8, 100, 10, "ethusd", 1551705072, 1591705072)
